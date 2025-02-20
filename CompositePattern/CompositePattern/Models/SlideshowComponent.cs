11b776c669237840b97e009b3ed280bd9d099f23
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2025-02-20 16:18:34 +0100

    Step 2: Implement leaves
    Add new classes for composite pattern and update UML diagram
    
    Updated `ClassDiagram.uxf` to modify coordinates, dimensions, and attributes of UML elements. Added new methods `setText()` and `AddPart()` to `UMLClass` elements.
    
    Added `Figure` class in `Figure.cs` with `Reference` property and overridden `Show()` method.
    
    Added abstract `SlideshowComponent` class in `SlideshowComponent.cs` with abstract `Show()` method and placeholder `AddPart()` method.
    
    Added abstract `SlideshowLeaf` class in `SlideshowLeaf.cs` inheriting from `SlideshowComponent`.
    
    Added `Text` class in `Text.cs` with `TheText` property and overridden `Show()` method.

---
+++ CompositePattern/CompositePattern/Models/SlideshowComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern.Models
+{
+    internal abstract class SlideshowComponent
+    {
+
+        public abstract void Show();
+
+        public void AddPart()
+        {
+            // todo implemenent
+        }
+    }
+}
