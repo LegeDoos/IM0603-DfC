612ef6841d9a92a03740bfcccd652e7106bb597e
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2025-02-20 16:00:50 +0100

    Add Composite Pattern project with initial structure
    Introduce new Visual Studio solution and project files for the Composite Pattern project. Add a `Program.cs` with a basic `Main` method. Include a UML class diagram (`ClassDiagram.uxf`) representing the Composite Pattern components.

---
+++ CompositePattern/CompositePattern/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CompositePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! This is my Composite Pattern exercise!");
+
+        }
+    }
+}
